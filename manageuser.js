ab1b7a1a8a3a9aeebe2ea11cc541acd5ccea4b43
kylee chapman <chappie1@byui.edu>
2023-01-19 15:38:42 -0700

    added one time password

--- manageuser.js
+++ manageuser.js
@@ -45,13 +45,23 @@ function checkexpiredtoken(token){
         dataType: 'text' })
     }
 }
+function oneTimePassword(){
+    $.ajax({
+        type: 'POST',
+        url: 'https://dev.stedi.me/twofactorlogin/2817557035',
+        data: JSON.stringify({userName, password}),
+       contentType: "application/text",
+        dataType: 'text'
+    });
+
+}
 
 function userlogin(){
     setuserpassword();
     setusername();
     $.ajax({
         type: 'POST',
-        url: '/login',
+        url: 'https://dev.stedi.me/login',
         data: JSON.stringify({userName, password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
