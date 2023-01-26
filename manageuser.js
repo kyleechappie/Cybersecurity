d4581d815c4e87a6c1e3deb5f3ec786e3c08b2cc
kylee chapman <chappie1@byui.edu>
2023-01-26 13:36:38 -0700

    week 4

--- manageuser.js
+++ manageuser.js
@@ -11,10 +11,6 @@ function setusername(){
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
-    }
 }
 
 function setverifypassword(){
@@ -46,9 +42,10 @@ function checkexpiredtoken(token){
     }
 }
 function oneTimePassword(){
+    const phoneNumber = document.getElementById('username').value 
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/twofactorlogin/2817557035',
+        url: 'https://dev.stedi.me/twofactorlogin/' + phoneNumber,
         data: JSON.stringify({userName, password}),
        contentType: "application/text",
         dataType: 'text'
@@ -61,8 +58,8 @@ function userlogin(){
     setusername();
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber: '2817557035', oneTimePassword: '3573'}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
